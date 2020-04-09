c4ba76ceed1506a0dc011a0d295bacaf7b04c5d6
Wazarr94 <39807740+Wazarr94@users.noreply.github.com>
2020-04-10 00:39:42 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,21 @@
+function resolveBotInputs (player, args = {}) {
+    if (player.disc != null) {
+        var ball = discs[0];
+        var threshold = 2;
+        var input = 0;
+        if (player.disc.x - ball.x > threshold) input += Input.LEFT;
+        if (player.disc.x - ball.x < -threshold) input += Input.RIGHT;
+        if (player.disc.y - ball.y > threshold) input += Input.UP;
+        if (player.disc.y - ball.y < -threshold) input += Input.DOWN;
+        if ((player.disc.x - ball.x) ** 2 + (player.disc.y - ball.y) ** 2 <= (ball.radius + player.disc.radius + 0.1) ** 2) input += Input.SHOOT;
+        player.inputs = input;
+        return;
+    }
+}
+
+function playInputs (player, args = {}) {
+    if (player.disc != null) {
+        player.inputs = args.inputArray[currentFrame];
+        return;
+    }
+}
