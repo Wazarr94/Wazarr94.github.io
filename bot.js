92bcab60ad052477f294698e9b04f102a032684a
Wazarr94 <39807740+Wazarr94@users.noreply.github.com>
2020-04-19 23:55:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-function resolveBotInputs (player, args = {}) {
+function resolveBotInputs1 (player, args = {}) {
     if (player.disc != null) {
         var ball = discs[0];
         var threshold = 2;
@@ -8,6 +8,22 @@ function resolveBotInputs (player, args = {}) {
         if (player.disc.y - ball.y > threshold) input += Input.UP;
         if (player.disc.y - ball.y < -threshold) input += Input.DOWN;
         if ((player.disc.x - ball.x) ** 2 + (player.disc.y - ball.y) ** 2 <= (ball.radius + player.disc.radius + 0.1) ** 2) input += Input.SHOOT;
+        player.inputs = input;
+        return;
+    }
+}
+
+function resolveBotInputs2 (player, args = {}) {
+    if (player.disc != null) {
+        var ball = discs[0];
+        var threshold = 2;
+        var input = 0;
+        if (currentFrame % 10 == 0) input += Input.UP;
+        if (player.disc.x - ball.x > threshold) input += Input.LEFT;
+        if (player.disc.x - ball.x < -threshold) input += Input.RIGHT;
+        if (player.disc.y - ball.y > threshold) input += Input.UP;
+        if (player.disc.y - ball.y < -threshold) input += Input.DOWN;
+        if ((player.disc.x - ball.x) ** 2 + (player.disc.y - ball.y) ** 2 <= (ball.radius + player.disc.radius + 0.05) ** 2) input += Input.SHOOT;
         player.inputs = input;
         return;
     }
